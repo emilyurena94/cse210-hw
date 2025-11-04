50c2496b6ade411082fe896013419d42ee68bdf8
emilyurena94 <emilyurena974@gmail.com>
2025-11-04 12:28:51 -0500

    Update Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,45 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        string otra = "yes";
+        
+        while (otra == "yes")
+        {
+            int number = randomGenerator.Next(1, 101);
+            int guess = 0;
+            int intentos = 0;
+
+            Console.WriteLine("\nWelcome to the Number Guessing Game!\n");
+            Console.WriteLine("\nGuess the number between 1 and 100!! Can you guess it?\n");
+
+            while (true)
+            {
+                Console.Write("\nEnter your guess: 7");
+                string input = Console.ReadLine();
+                guess = int.Parse(input);
+                intentos++;
+
+                if (guess < number)
+                {
+                    Console.WriteLine("Too low! Try again.");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Too high! Try again.");
+                }
+                else
+                {
+                    Console.WriteLine($"Congratulations! You've guessed the number {number} in {intentos} attempts.");
+                    break;
+                }
+            }
+            Console.Write("Do you want to play again? (yes/no): ");
+            otra = Console.ReadLine().ToLower();
+        }
+
+        Console.WriteLine("Thank you for playing!");
     }
 }
