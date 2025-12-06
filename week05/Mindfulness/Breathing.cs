484adac86128bf2f8c6bb82d919f51cd7c342f35
emilyurena94 <emilyurena974@gmail.com>
2025-12-06 08:20:07 -0500

    Add files via upload

--- week05/Mindfulness/Breathing.cs
+++ week05/Mindfulness/Breathing.cs
@@ -1,1 +1,29 @@
+using System;
+using System.Threading;
 
+public class BreathingActivity : Activity
+{
+    public BreathingActivity()
+        : base("Breathing", "This activity will help you relax by guiding you through slow breathing.\nFocus on your breath.")
+    {
+    }
+
+    public void Run(int duration)
+    {
+        DisplayStartingMessage();
+
+        int cycles = duration / 8;
+        for (int i = 0; i < cycles; i++)
+        {
+            Console.WriteLine("Breathe in...");
+            TimerCountdown(4);
+
+            Console.WriteLine("Now breathe out...");
+            TimerCountdown(4);
+
+            Console.WriteLine();
+        }
+
+        DisplayEndingMessage();
+    }
+}
